d02da6bdb24b4e811a60d0d25030713dcdced77d
Hila <hilabit@gmail.com>
2023-01-10 11:10:14 +0100

    added tests for creating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'core',
     'rest_framework',
     'drf_spectacular',
-    'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -135,5 +135,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openai.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
